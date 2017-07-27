567641db10dfce18a7351ba653e6ff6c995ca89c
Javed <jdecosta@gmail.com>
2017-07-27 15:15:57 +1000

    renderers logging

--- app/assets/javascripts/renderers.js
+++ app/assets/javascripts/renderers.js
@@ -2,6 +2,8 @@ var source   = $("#tile-template").html();
 var template = Handlebars.compile(source);
 var tile = $("#tile");
 
+console.log(tile);
+
 function popTile(obj) {
   console.log(obj);
 }

ca728c1cd09a90ade51a749471716b2d61be7ec0
brett <iambrettjohn@me.com>
2017-07-27 13:19:48 +1000

    interface updates

--- app/assets/javascripts/styling.js
+++ app/assets/javascripts/styling.js
@@ -6,3 +6,18 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+
+
+
+
+/* Markup for the Piano Tile Slider */
+
+function openTile() {
+    document.getElementById("tile").style.flex = 1;
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeTile() {
+    document.getElementById("tile").style.flex = 0;
+}

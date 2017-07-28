7f3a98d007027324fb8a997cb8260dc68ab19abe
Javed <jdecosta@gmail.com>
2017-07-28 12:01:33 +1000

    location model repair;

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,9 @@
-# class Location
-#
-#   geocoded_by :address, :ip_address
-#   reverse_geocoded_by :latitude, :longitude
-#   after_validation :geocode, :reverse_geocode, :if => :address_changed? || :latitude_changed? || :ip_address_changed?
-#
-#   attr_accessor :latitude, :longitude, :address, :ip_address
-#
-# end
+class Location
+
+  geocoded_by :address, :ip_address
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :geocode, :reverse_geocode, :if => :address_changed? || :latitude_changed? || :ip_address_changed?
+
+  attr_accessor :latitude, :longitude, :address, :ip_address
+
+end

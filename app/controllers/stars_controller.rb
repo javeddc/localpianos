440363053d8d911061bfda3364c044171aa28844
Javed <jdecosta@gmail.com>
2017-07-28 12:45:19 +1000

    = != == !!!!

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -7,16 +7,19 @@ class StarsController < ApplicationController
 
   def toggle
     star_to_toggle = Star.where(user_id: current_user.id, piano_id: params[:piano_id])[0]
-    if star_to_toggle = nil
+
+    if star_to_toggle == nil
       star_to_toggle = Star.create(user_id: current_user.id, piano_id: params[:piano_id])
     else
       star_to_toggle.destroy
     end
+
     if star_to_toggle != nil
       result = 'created'
     else
       result = 'destroyed'
     end
+
     render :json {result: result}
   end
 
